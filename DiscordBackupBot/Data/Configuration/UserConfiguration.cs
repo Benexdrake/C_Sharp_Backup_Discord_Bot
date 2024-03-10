c81da7f5fadaed24430b15d7819038f65d8bdb64
BeneX Drake <benex@hotmail.de>
2024-03-10 11:36:54 +0100

    deleted Model user

--- DiscordBackupBot/Data/Configuration/UserConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DiscordBackup.Bot.Data.Configuration;
-
-public class UserConfiguration : IEntityTypeConfiguration<User>
-{
-	public void Configure(EntityTypeBuilder<User> builder)
-	{
-		builder.HasMany(u => u.Messages).WithOne(m => m.User);
-	}
-}
