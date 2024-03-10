2cefa809cd4a6cf33daa52fb5fe172144ccb53c3
BeneX Drake <benex@hotmail.de>
2024-03-10 09:36:39 +0100

    no guild id more needed

--- DiscordBackupBot/Commands/Slash/BackupCommand.cs
+++ DiscordBackupBot/Commands/Slash/BackupCommand.cs
@@ -6,20 +6,20 @@ public class BackupCommand(IServiceProvider service, IConfiguration conf, ILogge
 	: InteractionModuleBase<SocketInteractionContext>
 {
 	private readonly DiscordSocketClient _client = service.GetRequiredService<DiscordSocketClient>();
-	private ulong _guildId = ulong.Parse(conf["GuildID"]);
 
 	public async Task Build()
 	{
-		var guild = _client.GetGuild(_guildId);
-		await guild.DeleteApplicationCommandsAsync();
+		foreach(var g in _client.Guilds)
+		{
+			await g.DeleteApplicationCommandsAsync();
 
-		var backup = new SlashCommandBuilder()
+			var backup = new SlashCommandBuilder()
 					.WithName("backup")
 					.WithDescription("backup service")
 					.AddOption(new SlashCommandOptionBuilder().WithName("choose").WithDescription("choose between true or false").WithType(ApplicationCommandOptionType.Boolean));
-
-		await guild.CreateApplicationCommandAsync(backup.Build());
-		Logger.LogInformation("Slash Command Backup");
+			await g.CreateApplicationCommandAsync(backup.Build());
+			Logger.LogInformation("Slash Command Backup");
+		}
 	}
 
 	[SlashCommand("backup", "backup", true, RunMode.Async)]
