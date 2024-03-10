5ded1da03366f9f683ad0e0bf8ead5701031df14
BeneX Drake <benex@hotmail.de>
2024-03-10 17:58:19 +0100

    change place

---
+++ DiscordBackupBot/Events/CommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using DiscordBackup.Bot.DL;
+
+namespace DiscordBackup.Bot.Events;
+
+public class CommandHandler(IServiceProvider Services, IConfiguration Config, ILogger<CommandHandler> Logger)
+{
+    private readonly BackupLogic backupLogic = Services.GetRequiredService<BackupLogic>();
+
+    public async Task SlashCommandExecuted(SocketSlashCommand arg)
+    {
+        if (arg.Channel is SocketTextChannel)
+        {
+            switch (arg.CommandName.ToLower())
+            {
+                case "backup":
+                        await backupLogic.InsertUpdateChannel(arg);
+                    break;
+                case "add_backup_server":
+                    await backupLogic.BackupGuildHandler(arg);
+                    break;
+            }
+        }
+    }
+
+    public async Task UserCommandExecuted(SocketUserCommand arg)
+    {
+        Logger.LogInformation("Executed: {arg}", arg.CommandName);
+    }
+}
