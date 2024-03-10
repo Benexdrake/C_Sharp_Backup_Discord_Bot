1733397085662ae84ba72ae6058b4a01de5e2dc0
BeneX Drake <benex@hotmail.de>
2024-03-10 17:58:53 +0100

    adding events

---
+++ DiscordBackupBot/Events/JoinedHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace DiscordBackup.Bot.Events;
+
+public class JoinedHandler(IServiceProvider Services, IConfiguration Config, ILogger<BackupBot> Logger)
+{
+	private readonly DiscordSocketClient _client = Services.GetRequiredService<DiscordSocketClient>();
+	public async Task UserJoined(SocketGuildUser arg)
+	{
+		
+
+		if (_client.CurrentUser.Id == arg.Guild.Owner.Id)
+		if (arg.Guild.Users.Count == 2)
+		{
+			var permissions = new GuildPermissions(administrator: true);
+
+			var role = await arg.Guild.CreateRoleAsync("admin", permissions, color: Color.Red);
+			await arg.AddRoleAsync(role);
+		}
+	}
+
+	public async Task JoinedGuild(SocketGuild guild)
+	{
+		if (guild.Name.Equals("backup"))
+		{
+			foreach (var channel in guild.Channels)
+			{
+				if (channel is SocketTextChannel && channel is not SocketVoiceChannel)
+				{
+					var c = channel as SocketTextChannel;
+					var invite = await c.CreateInviteAsync();
+
+					var webhook = new DiscordWebhookClient(Config["Webhook"]);
+					await webhook.SendMessageAsync(invite.Url);
+				}
+			}
+		}
+	}
+}
---
+++ DiscordBackupBot/Events/MessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using DiscordBackup.Bot.DL;
+
+namespace DiscordBackup.Bot.Events;
+
+public class MessageHandler(IServiceProvider Services, IConfiguration Config, ILogger<BackupBot> Logger)
+{
+	private readonly BackupLogic _backupLogic = Services.GetRequiredService<BackupLogic>();
+
+	public async Task MessageDeleted(Cacheable<IMessage, ulong> arg1, Cacheable<IMessageChannel, ulong> arg2)
+	{
+		Logger.LogInformation("Message: {arg1} - Deleted", arg1.Id);
+	}
+
+	public async Task MessageCreated(SocketMessage arg)
+	{
+		if (arg.Author.IsBot)
+			return;
+		await _backupLogic.MessageHandler(arg);
+		// backup Logic for sending  Messages to Backup Server
+	}
+}
