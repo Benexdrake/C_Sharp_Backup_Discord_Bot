757e9518529f08c0c52e5d405b030ea71fb05863
BeneX Drake <benex@hotmail.de>
2024-03-10 09:51:44 +0100

    Deleting User, every Message has a Channel N Message 1 Channel

--- DiscordBackupBot/Data/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DiscordBackup.Bot.Data.Models;
-
-public class User
-{
-    public int Id { get; set; }
-    public bool IsBackup { get; set; }
-    public bool IsAdmin { get; set; }
-    public ICollection<Message> Messages { get; set; } = [];
-}
